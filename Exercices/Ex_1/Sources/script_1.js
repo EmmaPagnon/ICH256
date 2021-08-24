e72c93b2d50f5140d47087c1973b2d1fd145565e
Pagnon <emma.pagnon@ceff.ch>
2021-08-24 11:13:49 +0200

    Exercice 1

---
+++ Exercices/Ex_1/Sources/script_1.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#content,p").css("background-color", "gray")
+    $("h1,h2").css("color", "red")
+    $("h1,h2,p").css("border-color", "red")
+    $("h2.anecdote+p").css("color","white")
+    $("body").css("color","black")
+});
