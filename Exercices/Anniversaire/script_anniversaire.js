6534679f084b673b5ed7f8b849655ddffc366eee
Pagnon <emma.pagnon@ceff.ch>
2021-09-07 13:41:54 +0200

    Anniversaire

--- Exercices/Anniversaire/script_anniversaire.js
+++ Exercices/Anniversaire/script_anniversaire.js
@@ -1,33 +1,65 @@
 $(function(){
 
-    $("h1.centered,.first,.second,.third,.final,img#second_image").hide()
-    $("body").click(function(){
-            $("h1.centered").animate({
-                top: '250px',
-                height: 'toggle',
-                width: '450px'
-            });
-        $("body").click(function(){
-            $("h1.centered").hide()
-            $(".first").show();
 
-            $("body").click(function(){
-                $(".first").hide()
-                $(".second").show();
+    var ml4 = {};
+    ml4.opacityIn = [0,1];
+    ml4.scaleIn = [0.2, 1];
+    ml4.scaleOut = 3;
+    ml4.durationIn = 1000;
+    ml4.durationOut = 1000;
+    ml4.delay = 500;
 
-                $("body").click(function(){
-                    $(".second").hide()
-                    $(".third").show();
+    anime.timeline({loop: true})
+        .add({
+            targets: '.ml4 .letters-1',
+            opacity: ml4.opacityIn,
+            scale: ml4.scaleIn,
+            duration: ml4.durationIn
+        }).add({
+        targets: '.ml4 .letters-1',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4',
+        opacity: 0,
+        duration: 500,
 
-                    $("body").click(function(){
-                        $(".third,img#first_image").hide()
-                        $(".final,img#second_image").show();
+        delay: 500
+    });
 
-                    });
-                });
-            });
-        });
-    })
+
+
+
+
+
+
 
 });
 
---
+++ Exercices/Calc/Calc.js
@@ -0,0 +1,8 @@
+
+function generateRandomNumbers(min, max) {
+    var min = Math.ceil();
+    var max = Math.floor();
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
